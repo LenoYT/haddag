73df76644e7c5c39adcb588a664f3ad70cb29300
Leno <leon@wincamp.pl>
2024-08-27 12:24:30 +0200

    I created a Bug - plz help

--- src/main/java/pl/supercraft/leno/minecraft/mods/fabric/haddag/block/ModBlocks.java
+++ src/main/java/pl/supercraft/leno/minecraft/mods/fabric/haddag/block/ModBlocks.java
@@ -3,6 +3,7 @@ package pl.supercraft.leno.minecraft.mods.fabric.haddag.block;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.block.MapColor;
 import net.minecraft.block.enums.Instrument;
 import net.minecraft.item.BlockItem;
@@ -11,6 +12,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import pl.supercraft.leno.minecraft.mods.fabric.haddag.Haddag;
 
 public class ModBlocks {
@@ -22,12 +24,13 @@ public class ModBlocks {
             .collidable(true)
             .requiresTool()
     ));
-    public static final Block RUBY_ORE = registerBlock("ruby_block", new Block(FabricBlockSettings.create()
+    public static final Block RUBY_ORE = registerBlock("ruby_block", new ExperienceDroppingBlock(FabricBlockSettings.create()
             .mapColor(MapColor.TERRACOTTA_GRAY)
             .strength(5.0F, 10.0F)
             .sounds(BlockSoundGroup.AMETHYST_BLOCK)
             .collidable(true)
-            .requiresTool()
+            .requiresTool(),
+            UniformIntProvider.create(2, 5)
     ));
 
     private static Block registerBlock(String name, Block block) {
